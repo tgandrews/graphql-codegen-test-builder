c4414e8d7396fb92af533e72b7dfb05fbfa8a3aa
Tom Andrews <git@tomandrews.co.uk>
2026-04-27 22:26:23 +0100

    Refactor plugin golden tests into scenario blocks (#14)
    * Refactor plugin golden tests by scenario
    
    * Restore built-in scalar golden coverage

--- src/plugin.spec.ts
+++ src/plugin.spec.ts
@@ -22,349 +22,358 @@ const runPlugin = async (query: string, schemaString: string, config: Partial<Co
 };
 
 describe('plugin', () => {
-  it('should generate a builder class for a query', async () => {
-    const schema = `
-      type Query {
-        me: User!
-      }
-
-      type User {
-        name: String!
-      }
-    `;
-    const query = `
-      query GetUser {
-        me {
-          name
-        }
-      }
-    `;
-    const result = await runPlugin(query, schema);
-    expect(result).toEqual(
-      prettify(
-        `type MockUserType = {
-        name: string;
-      }
-
-      class MockGetUserQueryBuilder {
-        private me: MockUserType = {
-          name: '',
-        };
-
-        havingMe(me: MockUserType): this {
-          this.me = me;
-          return this;
+  describe('query builders', () => {
+    it('should generate a builder class for a query', async () => {
+      const schema = `
+        type Query {
+          me: User!
         }
 
-        build(): MockedResponse<GetUserQueryResponse, GetUserQueryVariables> {
-          return {
-            request: {
-              query: GetUserQueryDocument,
-            },
-            result: {
-              data: {
-                __typename: 'Query',
-                me: {
-                  __typename: 'User',
-                  name: this.me.name,
+        type User {
+          name: String!
+        }
+      `;
+      const query = `
+        query GetUser {
+          me {
+            name
+          }
+        }
+      `;
+      const result = await runPlugin(query, schema);
+      expect(result).toEqual(
+        prettify(
+          `type MockUserType = {
+          name: string;
+        }
+
+        class MockGetUserQueryBuilder {
+          private me: MockUserType = {
+            name: '',
+          };
+
+          havingMe(me: MockUserType): this {
+            this.me = me;
+            return this;
+          }
+
+          build(): MockedResponse<GetUserQueryResponse, GetUserQueryVariables> {
+            return {
+              request: {
+                query: GetUserQueryDocument,
+              },
+              result: {
+                data: {
+                  __typename: 'Query',
+                  me: {
+                    __typename: 'User',
+                    name: this.me.name,
+                  }
                 }
               }
-            }
-          } as const
-        }
-      }`
-      )
-    );
+            } as const
+          }
+        }`
+        )
+      );
+    });
   });
 
-  it('should generate a builder class for a mutation', async () => {
-    const schema = `
-      type Mutation {
-        createUser(input: CreateUserInput!): User!
-      }
-
-      input CreateUserInput {
-        name: String!
-        age: Int
-      }
-
-      type User {
-        name: String!
-        age: Int
-      }
-    `;
-    const query = `
-      mutation CreateUser($input: CreateUserInput!) {
-        createUser(input: $input) {
-          name
-        }
-      }
-    `;
-    const result = await runPlugin(query, schema);
-    expect(result).toEqual(
-      prettify(
-        `type MockUserType = {
-        name: string;
-        age: number | null;
-      }
-
-      type MockCreateUserInputType = {
-        name: string;
-        age: number | null;
-      }
-
-      class MockCreateUserMutationBuilder {
-        private input: MockCreateUserInputType = {
-          name: '',
-          age: null,
-        };
-
-        private createUser: MockUserType = {
-          name: '',
-          age: null,
-        };
-
-        forInput(input: MockCreateUserInputType): this {
-          this.input = input;
-          return this;
+  describe('mutation builders', () => {
+    it('should generate a builder class for a mutation', async () => {
+      const schema = `
+        type Mutation {
+          createUser(input: CreateUserInput!): User!
         }
 
-        havingCreateUser(createUser: MockUserType): this {
-          this.createUser = createUser;
-          return this;
+        input CreateUserInput {
+          name: String!
+          age: Int
         }
 
-        build(): MockedResponse<CreateUserMutationResponse, CreateUserMutationVariables> {
-          return {
-            request: {
-              query: CreateUserMutationDocument,
-              variables: {
-                input: this.input,
-              }
-            },
-            result: {
-              data: {
-                __typename: 'Mutation',
-                createUser: {
-                  __typename: 'User',
-                  name: this.createUser.name,
+        type User {
+          name: String!
+          age: Int
+        }
+      `;
+      const query = `
+        mutation CreateUser($input: CreateUserInput!) {
+          createUser(input: $input) {
+            name
+          }
+        }
+      `;
+      const result = await runPlugin(query, schema);
+      expect(result).toEqual(
+        prettify(
+          `type MockUserType = {
+          name: string;
+          age: number | null;
+        }
+
+        type MockCreateUserInputType = {
+          name: string;
+          age: number | null;
+        }
+
+        class MockCreateUserMutationBuilder {
+          private input: MockCreateUserInputType = {
+            name: '',
+            age: null,
+          };
+
+          private createUser: MockUserType = {
+            name: '',
+            age: null,
+          };
+
+          forInput(input: MockCreateUserInputType): this {
+            this.input = input;
+            return this;
+          }
+
+          havingCreateUser(createUser: MockUserType): this {
+            this.createUser = createUser;
+            return this;
+          }
+
+          build(): MockedResponse<CreateUserMutationResponse, CreateUserMutationVariables> {
+            return {
+              request: {
+                query: CreateUserMutationDocument,
+                variables: {
+                  input: this.input,
+                }
+              },
+              result: {
+                data: {
+                  __typename: 'Mutation',
+                  createUser: {
+                    __typename: 'User',
+                    name: this.createUser.name,
+                  }
                 }
               }
-            }
-          } as const
-        }
-      }`
-      )
-    );
+            } as const
+          }
+        }`
+        )
+      );
+    });
   });
 
-  it('should inline small classes (<=3 properties)', async () => {
-    const schema = `
-      type Query {
-        me: User!
-      }
-
-      type User {
-        name: String!
-        age: Int
-        email: String
-      }
-    `;
-    const query = `
-      query GetUser {
-        me {
-          name
-          age
-          email
-        }
-      }
-    `;
-    const result = await runPlugin(query, schema);
-    expect(result).toEqual(
-      prettify(
-        `
-      type MockUserType = {
-        name: string;
-        age: number | null;
-        email: string | null;
-      }
-
-      class MockGetUserQueryBuilder {
-        private me: MockUserType = {
-          name: '',
-          age: null,
-          email: null,
-        };
-
-        havingMe(me: MockUserType): this {
-          this.me = me;
-          return this;
+  describe('inline small types', () => {
+    it('should inline small classes (<=3 properties)', async () => {
+      const schema = `
+        type Query {
+          me: User!
         }
 
-        build(): MockedResponse<GetUserQueryResponse, GetUserQueryVariables> {
-          return {
-            request: {
-              query: GetUserQueryDocument,
-            },
-            result: {
-              data: {
-                __typename: 'Query',
-                me: {
-                  __typename: 'User',
-                  name: this.me.name,
-                  age: this.me.age,
-                  email: this.me.email,
+        type User {
+          name: String!
+          age: Int
+          email: String
+        }
+      `;
+      const query = `
+        query GetUser {
+          me {
+            name
+            age
+            email
+          }
+        }
+      `;
+      const result = await runPlugin(query, schema);
+      expect(result).toEqual(
+        prettify(
+          `
+        type MockUserType = {
+          name: string;
+          age: number | null;
+          email: string | null;
+        }
+
+        class MockGetUserQueryBuilder {
+          private me: MockUserType = {
+            name: '',
+            age: null,
+            email: null,
+          };
+
+          havingMe(me: MockUserType): this {
+            this.me = me;
+            return this;
+          }
+
+          build(): MockedResponse<GetUserQueryResponse, GetUserQueryVariables> {
+            return {
+              request: {
+                query: GetUserQueryDocument,
+              },
+              result: {
+                data: {
+                  __typename: 'Query',
+                  me: {
+                    __typename: 'User',
+                    name: this.me.name,
+                    age: this.me.age,
+                    email: this.me.email,
+                  }
                 }
               }
-            }
-          } as const
-        }
-      }`
-      )
-    );
+            } as const
+          }
+        }`
+        )
+      );
+    });
   });
 
-  it('should support re-used queries selecting different fields', async () => {
-    const schema = `
-      type Query {
-        me: User!
-      }
-
-      type User {
-        name: String!
-        age: Int!
-        email: String!
-      }
-    `;
-    const query = `
-      query GetUserName {
-        me {
-          name
-        }
-      }
-
-      query GetUserEmail {
-        me {
-          email
-        }
-      }
-    `;
-    const result = await runPlugin(query, schema);
-    expect(result).toEqual(
-      prettify(
-        `
-      type MockUserType = {
-        name: string;
-        email: string;
-      }
-
-      type GetUserNameUserType = Pick<MockUserType, "name">;
-
-      class MockGetUserNameQueryBuilder {
-        private me: GetUserNameUserType = {
-          name: '',
-        };
-
-        havingMe(me: GetUserNameUserType): this {
-          this.me = me;
-          return this;
+  describe('reused query selections', () => {
+    it('should support reused queries selecting different fields', async () => {
+      const schema = `
+        type Query {
+          me: User!
         }
 
-        build(): MockedResponse<GetUserNameQueryResponse, GetUserNameQueryVariables> {
-          return {
-            request: {
-              query: GetUserNameQueryDocument,
-            },
-            result: {
-              data: {
-                __typename: 'Query',
-                me: {
-                  __typename: 'User',
-                  name: this.me.name,
+        type User {
+          name: String!
+          age: Int!
+          email: String!
+        }
+      `;
+      const query = `
+        query GetUserName {
+          me {
+            name
+          }
+        }
+
+        query GetUserEmail {
+          me {
+            email
+          }
+        }
+      `;
+      const result = await runPlugin(query, schema);
+      expect(result).toEqual(
+        prettify(
+          `
+        type MockUserType = {
+          name: string;
+          email: string;
+        }
+
+        type GetUserNameUserType = Pick<MockUserType, "name">;
+
+        class MockGetUserNameQueryBuilder {
+          private me: GetUserNameUserType = {
+            name: '',
+          };
+
+          havingMe(me: GetUserNameUserType): this {
+            this.me = me;
+            return this;
+          }
+
+          build(): MockedResponse<GetUserNameQueryResponse, GetUserNameQueryVariables> {
+            return {
+              request: {
+                query: GetUserNameQueryDocument,
+              },
+              result: {
+                data: {
+                  __typename: 'Query',
+                  me: {
+                    __typename: 'User',
+                    name: this.me.name,
+                  }
                 }
               }
-            }
-          } as const
-        }
-      }
-
-      type GetUserEmailUserType = Pick<MockUserType, "email">;
-
-      class MockGetUserEmailQueryBuilder {
-        private me: GetUserEmailUserType = {
-          email: '',
-        };
-
-        havingMe(me: GetUserEmailUserType): this {
-          this.me = me;
-          return this;
+            } as const
+          }
         }
 
-        build(): MockedResponse<GetUserEmailQueryResponse, GetUserEmailQueryVariables> {
-          return {
-            request: {
-              query: GetUserEmailQueryDocument,
-            },
-            result: {
-              data: {
-                __typename: 'Query',
-                me: {
-                  __typename: 'User',
-                  email: this.me.email,
+        type GetUserEmailUserType = Pick<MockUserType, "email">;
+
+        class MockGetUserEmailQueryBuilder {
+          private me: GetUserEmailUserType = {
+            email: '',
+          };
+
+          havingMe(me: GetUserEmailUserType): this {
+            this.me = me;
+            return this;
+          }
+
+          build(): MockedResponse<GetUserEmailQueryResponse, GetUserEmailQueryVariables> {
+            return {
+              request: {
+                query: GetUserEmailQueryDocument,
+              },
+              result: {
+                data: {
+                  __typename: 'Query',
+                  me: {
+                    __typename: 'User',
+                    email: this.me.email,
+                  }
                 }
               }
-            }
-          } as const
-        }
-      }`
-      )
-    );
+            } as const
+          }
+        }`
+        )
+      );
+    });
   });
 
-  it('should support all built-in scalars (String, Int, Boolean, Float, ID)', async () => {
-    const schema = `
-      type Query {
-        me: User!
-      }
-
-      type Mutation {
-        deleteUser(input: DeleteUserInput!): User!
-      }
-
-      input DeleteUserInput {
-        id: ID!
-        soft: Boolean
-      }
-
-      type User {
-        id: ID!
-        name: String!
-        active: Boolean!
-        score: Float!
-        age: Int!
-      }
-    `;
-    const query = `
-      query GetUser {
-        me {
-          id
-          name
-          active
-          score
-          age
+  describe('built-in scalars', () => {
+    it('should support all built-in scalars (String, Int, Boolean, Float, ID)', async () => {
+      const schema = `
+        type Query {
+          me: User!
         }
-      }
 
-      mutation DeleteUser($input: DeleteUserInput!) {
-        deleteUser(input: $input) {
-          id
+        type Mutation {
+          deleteUser(input: DeleteUserInput!): User!
         }
-      }
-    `;
-    const result = await runPlugin(query, schema);
-    expect(result).toEqual(
-      prettify(
-        `type MockUserType = {
+
+        input DeleteUserInput {
+          id: ID!
+          soft: Boolean
+        }
+
+        type User {
+          id: ID!
+          name: String!
+          active: Boolean!
+          score: Float!
+          age: Int!
+        }
+      `;
+      const query = `
+        query GetUser {
+          me {
+            id
+            name
+            active
+            score
+            age
+          }
+        }
+
+        mutation DeleteUser($input: DeleteUserInput!) {
+          deleteUser(input: $input) {
+            id
+          }
+        }
+      `;
+      const result = await runPlugin(query, schema);
+      expect(result).toEqual(
+        prettify(
+          `type MockUserType = {
           id: string;
           name: string;
           active: boolean;
@@ -455,75 +464,78 @@ describe('plugin', () => {
             } as const
           }
         }`
-      )
-    );
+        )
+      );
+    });
   });
 
-  it('should support user defined classes with a named export', async () => {
-    const schema = `
-      type Query {
-        users: [User!]!
-      }
-
-      type User {
-        name: String!
-        age: Int!
-        deletedAt: String!
-        otherField: String!
-        # Unused field to ensure user defined class is used
-        createdAt: String!
-      }
-    `;
-    const query = `
-      query GetUsers {
-        users {
-          name
-          age
-          deletedAt
-          otherField
-        }
-      }
-    `;
-    const config: Partial<Config> = {
-      userDefinedClasses: {
-        User: { path: './userModel', exportName: 'MockUserType' },
-      },
-    };
-    const result = await runPlugin(query, schema, config);
-    expect(result).toEqual(
-      prettify(
-        `
-      import { MockUserType } from './userModel';
-
-      class MockGetUsersQueryBuilder {
-        private users: MockUserType[] = [];
-
-        havingUsers(users: MockUserType[]): this {
-          this.users = users;
-          return this;
+  describe('user defined classes', () => {
+    it('should support user defined classes with a named export', async () => {
+      const schema = `
+        type Query {
+          users: [User!]!
         }
 
-        build(): MockedResponse<GetUsersQueryResponse, GetUsersQueryVariables> {
-          return {
-            request: {
-              query: GetUsersQueryDocument,
-            },
-            result: {
-              data: {
-                __typename: 'Query',
-                users: this.users.map(item => ({
-                  __typename: 'User',
-                  name: item.name,
-                  age: item.age,
-                  deletedAt: item.deletedAt,
-                  otherField: item.otherField,
-                })),
+        type User {
+          name: String!
+          age: Int!
+          deletedAt: String!
+          otherField: String!
+          # Unused field to ensure user defined class is used
+          createdAt: String!
+        }
+      `;
+      const query = `
+        query GetUsers {
+          users {
+            name
+            age
+            deletedAt
+            otherField
+          }
+        }
+      `;
+      const config: Partial<Config> = {
+        userDefinedClasses: {
+          User: { path: './userModel', exportName: 'MockUserType' },
+        },
+      };
+      const result = await runPlugin(query, schema, config);
+      expect(result).toEqual(
+        prettify(
+          `
+        import { MockUserType } from './userModel';
+
+        class MockGetUsersQueryBuilder {
+          private users: MockUserType[] = [];
+
+          havingUsers(users: MockUserType[]): this {
+            this.users = users;
+            return this;
+          }
+
+          build(): MockedResponse<GetUsersQueryResponse, GetUsersQueryVariables> {
+            return {
+              request: {
+                query: GetUsersQueryDocument,
+              },
+              result: {
+                data: {
+                  __typename: 'Query',
+                  users: this.users.map(item => ({
+                    __typename: 'User',
+                    name: item.name,
+                    age: item.age,
+                    deletedAt: item.deletedAt,
+                    otherField: item.otherField,
+                  })),
+                }
               }
-            }
-          } as const
-        }
-      }`
-      )
-    );
+            } as const
+          }
+        }`
+        )
+      );
+    });
   });
 });
